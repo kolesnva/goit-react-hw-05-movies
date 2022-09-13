d52bb29ca923b556da0cf923cfea1b09fa6f0169
kolesnva <kolesnychenko_v@icloud.com>
2022-09-13 22:04:01 +0300

    installed all needed libs

---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,11 @@
+import { List, Item } from './MovieListStyled';
+
+function MovieList() {
+  return (
+    <List>
+      <Item>Some text inside</Item>
+    </List>
+  );
+}
+
+export default MovieList;
